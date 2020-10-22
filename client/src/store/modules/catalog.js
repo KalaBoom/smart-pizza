ed8c27962d4d65ffc2ec9e4eab1b59a0c539a6ce
Eugene <gaster334@gmail.com>
2020-10-22 13:53:33 +0300

    fix store getters products

--- client/src/store/modules/catalog.js
+++ client/src/store/modules/catalog.js
@@ -7,29 +7,29 @@ export default {
         } 
     },
     state: {
-        // products: [            
-        //     {id: 1, type: 'пицца', title: 'Перерони', 
-        //     ingredients: ['Сыр моцарелла', 'Соус карри', 'Баварские колбаски'], cost: 300, count: 0, discount: false},
+        products: [            
+            {id: 1, type: 'пицца', title: 'Перерони', 
+            ingredients: ['Сыр моцарелла', 'Соус карри', 'Баварские колбаски'], cost: 300, count: 0, discount: false},
 
-        //     {id: 2, type: 'пицца', title: 'Маргарита', 
-        //     ingredients: ['Ветчина', 'Соус Чесночный', 'Сыр моцарелла', 'Огурцы'], cost: 500, count: 0, discount: 20},
+            {id: 2, type: 'пицца', title: 'Маргарита', 
+            ingredients: ['Ветчина', 'Соус Чесночный', 'Сыр моцарелла', 'Огурцы'], cost: 500, count: 0, discount: 20},
 
-        //     {id: 3, type: 'пицца', title: 'Ананасовая', 
-        //     ingredients: ['Голубой сыр', 'Пепперони', 'Сыр моцарелла', 'Соус Чесночный'], cost: 700, count: 0, discount: false},
+            {id: 3, type: 'пицца', title: 'Ананасовая', 
+            ingredients: ['Голубой сыр', 'Пепперони', 'Сыр моцарелла', 'Соус Чесночный'], cost: 700, count: 0, discount: false},
 
-        //     {id: 4, type: 'пицца', title: 'Гавайская', 
-        //     ingredients: ['Соус барбекю', 'Пармезан', 'Сыр моцарелла', 'Халапеньо', 'Баварские колбаски'], cost: 800, count: 0, discount: 5},
+            {id: 4, type: 'пицца', title: 'Гавайская', 
+            ingredients: ['Соус барбекю', 'Пармезан', 'Сыр моцарелла', 'Халапеньо', 'Баварские колбаски'], cost: 800, count: 0, discount: 5},
 
-        //     {id: 5, type: 'пицца', title: 'Сырная', 
-        //     ingredients: ['Сладкий перец', 'Сыр моцарелла', 'Лук', 'Оливки', 'Томаты', 'Шампиньоны', 'Томатный соус'], cost: 400, count: 0, discount: false},
+            {id: 5, type: 'пицца', title: 'Сырная', 
+            ingredients: ['Сладкий перец', 'Сыр моцарелла', 'Лук', 'Оливки', 'Томаты', 'Шампиньоны', 'Томатный соус'], cost: 400, count: 0, discount: false},
 
-        //     {id: 6, type: 'закуска', title: 'сэндвич-ролл куриный', cost: 199, count: 0, discount: 10},
-        //     {id: 7, type: 'закуска', title: 'картофель фри', cost: 149, count: 0, discount: false},
-        //     {id: 8, type: 'закуска', title: 'куриные крылья BBQ', cost: 199, count: 0, discount: false},
-        //     {id: 9, type: "напиток", title: "Кофе Латте", cost: 115, count: 0, discount: false},
-        //     {id: 10, type: "напиток", title: "Пепси", cost: 119, count: 0, discount: 5}
-        // ],
-        products: [],
+            {id: 6, type: 'закуска', title: 'сэндвич-ролл куриный', cost: 199, count: 0, discount: 10},
+            {id: 7, type: 'закуска', title: 'картофель фри', cost: 149, count: 0, discount: false},
+            {id: 8, type: 'закуска', title: 'куриные крылья BBQ', cost: 199, count: 0, discount: false},
+            {id: 9, type: "напиток", title: "Кофе Латте", cost: 115, count: 0, discount: false},
+            {id: 10, type: "напиток", title: "Пепси", cost: 119, count: 0, discount: 5}
+        ],
+        //products: [],
         cart: []
     },
     mutations: {
@@ -59,14 +59,14 @@ export default {
         allProducts(state) {
             return state.products
         },
-        productsPizza(state) {
-            return state.products.filter(product => product.type === 'пицца')
+        productsPizza(state, getters) {
+            return state.products.filter(product => getProductType(getters.cart, getters.cartIds, product, 'пицца'))
         },
-        productsSnack(state) {
-            return state.products.filter(product => product.type === 'закуска')
+        productsSnack(state, getters) {
+            return state.products.filter(product => getProductType(getters.cart, getters.cartIds, product, 'закуска'))
         },
-        productsDrink(state) {
-            return state.products.filter(product => product.type === 'напиток')
+        productsDrink(state, getters) {
+            return state.products.filter(product => getProductType(getters.cart, getters.cartIds, product, 'напиток'))
         },
         cart(state) {
             return state.cart
@@ -84,4 +84,9 @@ export default {
             return state.cart.reduce((acc, item) => item.count + acc, 0)
         }
     }
+}
+
+function getProductType(cart, cartIds, product, type) {
+    if(cartIds.includes(product.id)) product = cart.find(item => item.id === product.id)
+    return product.type === type
 }
